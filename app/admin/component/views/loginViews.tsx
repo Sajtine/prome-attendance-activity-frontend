caa8fe6c55bd6f78b30fc8dd9d39181757f81ec2
Sajtine <justinegomez777@gmail.com>
2026-02-24 15:49:17 +0800

    modify to use the railway backend

--- app/admin/component/views/loginViews.tsx
+++ app/admin/component/views/loginViews.tsx
@@ -19,7 +19,7 @@ const LoginViews = () => {
     setMessage({ type: "", text: "" });
 
     try {
-      const response = await axios.post("http://localhost:3000/admin/login", {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}admin/login`, {
         username,
         password,
       });
