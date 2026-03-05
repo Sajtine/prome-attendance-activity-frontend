bfb83c628c094ed77b7c6cc454e3ad0c8980990c
Sajtine <justinegomez777@gmail.com>
2026-03-05 11:28:24 +0800

    refactor: local socket to online url socket

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+// localhost:3000 for local backend
+// export const socket = io("http://localhost:3000", {
+//     transports: ["websocket"],
+// })
+
+export const socket = io(process.env.NEXT_PUBLIC_API_URL, {
     transports: ["websocket"],
-})
+});
